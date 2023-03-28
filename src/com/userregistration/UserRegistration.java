4ab8fe8dec5f222412e1d02323e99f4130b656c3
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-28 16:31:21 +0530

    ADDED_UC9

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -78,6 +78,42 @@ public class UserRegistration {
             System.out.println("Invalid password_rule4");
 
 
+        //uc9
+        //  1. abc@yahoo.com,
+        //• 2. abc-100@yahoo.com,
+        //• 3. abc.100@yahoo.com
+        //• 2. abc111@abc.com,
+        //• 4. abc-100@abc.net,
+        //• 5. abc.100@abc.com.au
+        //• 6. abc@1.com,
+        //• 7. abc@gmail.com.com
+        //• 8. abc+100@gmail.com
+        String text = "Here are some sample email addresses:"+
+                "        //  1. abc@yahoo.com,\n" +
+                "        //• 2. abc-100@yahoo.com,\n" +
+                "        //• 3. abc.100@yahoo.com\n" +
+                "        //• 4. abc111@abc.com,\n" +
+                "        //• 5. abc-100@abc.net,\n" +
+                "        //• 6. abc.100@abc.com.au\n" +
+                "        //• 7. abc@1.com,\n" +
+                "        //• 8. abc@gmail.com.com\n" +
+                "        //• 9. abc+100@gmail.com";
+
+        // Define the regular expression to match email addresses
+        String emailPattern = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}";
+
+        // Create a Pattern object from the regular expression
+        Pattern pattern8 = Pattern.compile(emailPattern);
+
+        // Use a Matcher object to find all occurrences of the pattern in the text
+        Matcher matcher8 = pattern8.matcher(text);
+
+        // Iterate over the matches and print each email address
+        System.out.println("Valid email are : ");
+        while (matcher8.find()) {
+            String email = matcher8.group();
+            System.out.println(email);
+        }
     }
 }
 
